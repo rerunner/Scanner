18800cf8409dca2e165e108c46f677dd3a00b2e7
rerunner <rerunner@example.com>
2024-03-28 17:39:39 +0100

    Removed disabled code

--- src/MachineControl/application/MachineControl.cpp
+++ src/MachineControl/application/MachineControl.cpp
@@ -24,15 +24,6 @@ namespace MachineControl
         Wafer newWafer; // load new wafer
         newWafer.PreAligned(); // Move wafer to prealigned state (will be event later)
 
-#if 0
-        Leveling::Leveling leveling; // Create leveling object
-        LevelingCommands::CommandExecutor executor(leveling); // Give commands to leveling object
-
-        //actual execution, can be moved to queue command processor
-        LevelingCommands::Command command; 
-        command = LevelingCommands::MeasureWafer{newWafer.GetId()}; // Command: Measure Wafer with uuid
-        std::visit(executor, command); // execute the command
-#endif
         // Leveling part
         {
             std::cout << "starting leveling measure heightmap command with curl" << std::endl;
